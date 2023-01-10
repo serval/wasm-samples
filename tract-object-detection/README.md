440d7ecbc4689289496904f700775206a0250673
Mark Christian <m@rkchristian.ca>
2023-01-10 12:51:52 -0800

    Simple object detection demo

---
+++ tract-object-detection/README.md
@@ -0,0 +1,51 @@
+# Object detection with Tract and Tensorflow MobileNet v2
+
+This sample is derived from the [tensorflow-mobilenet-v2](https://github.com/sonos/tract/tree/main/examples/tensorflow-mobilenet-v2) example in the [sonos/tract project](https://github.com/sonos/tract).
+
+## Important: Before compiling, obtaining the model
+
+MobileNet is a response to the ImageNet challenge. The goal is to categorize
+images and associate them with one of 1000 labels. In other words, recognize a
+dog, a cat, a rabbit, or a military uniform.
+
+See https://github.com/tensorflow/models/tree/master/research/slim/nets/mobilenet for more information.
+
+You will need to download the models. For instance:
+
+```sh
+wget https://storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.4_224.tgz && tar zxf mobilenet_v2_1.4_224.tgz
+```
+
+This expands a half-dozen files in the directory. The only files we care about are `imagenet_slim_labels.txt` and `mobilenet_v2_1.4_224_frozen.pb`, both of which will be automatically
+inlined into the tract-object-detection binary when it is compiled.
+
+## Running object detection locally
+
+This app will run object detection on any image given to it over stdin. There are a handful of example images in this repository:
+
+```sh
+cargo run < examples/grace_hopper.jpg
+```
+
+This should print:
+
+```
+military uniform (33.37%)
+suit (12.55%)
+bulletproof vest (9.98%)
+academic gown (5.73%)
+```
+
+## Compiling to WASM and running on Serval
+
+`release` builds are noticeably faster than `debug` builds:
+
+```sh
+cargo build --target wasm32-wasi --release
+```
+
+Then, from the `serval-mesh` project (assuming this repository is checked out as a sibling directory):
+
+```sh
+cargo run --bin pounce -- run ../wasm-samples/tract-object-detection/target/wasm32-wasi/release/tract-object-detection.wasm ../wasm-samples/tract-object-detection/examples/grace_hopper.jpg
+```
---
+++ tract-object-detection/src/main.rs
@@ -0,0 +1,67 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+
+use tract_tensorflow::prelude::{tract_itertools::Itertools, *};
+use tract_tensorflow::tract_hir::internal::anyhow;
+
+fn main() -> TractResult<()> {
+    // load the labels
+    let labels_str = include_str!("/Users/mark/Code/servals/research/tract/examples/tensorflow-mobilenet-v2/imagenet_slim_labels.txt");
+    let labels: Vec<_> = labels_str.lines().collect();
+
+    let mut model_data: &[u8] = include_bytes!("/Users/mark/Code/servals/research/tract/examples/tensorflow-mobilenet-v2/mobilenet_v2_1.4_224_frozen.pb");
+    let model = tract_tensorflow::tensorflow()
+        // load the model
+        .model_for_read(&mut model_data)?
+        // specify input type and shape
+        .with_input_fact(0, f32::fact([1, 224, 224, 3]).into())?
+        // optimize the model
+        .into_optimized()?
+        // make the model runnable and fix its inputs and outputs
+        .into_runnable()?;
+
+    // open image, resize it and make a Tensor out of it
+    if atty::is(atty::Stream::Stdin) {
+        return Err(anyhow!("Provide image data on stdin"));
+    }
+
+    let mut image_data: Vec<u8> = Vec::new();
+    let mut reader = BufReader::new(std::io::stdin());
+    let bytes_read = reader.read_to_end(&mut image_data)?;
+    if bytes_read == 0 {
+        return Err(anyhow!("Provide image data on stdin"));
+    }
+
+    let image = image::load_from_memory(&image_data)?;
+    let resized =
+        image::imageops::resize(&image, 224, 224, ::image::imageops::FilterType::Triangle);
+    let image: Tensor = tract_ndarray::Array4::from_shape_fn((1, 224, 224, 3), |(_, y, x, c)| {
+        resized[(x as _, y as _)][c] as f32 / 255.0
+    })
+    .into();
+
+    // run the model on the input
+    let result = model.run(tvec!(image.into()))?;
+
+    // take the top N results and display them, along with their label
+    const MAX_RESULTS: usize = 10;
+    const MIN_CONFIDENCE: f32 = 0.05;
+
+    let sorted = result[0]
+        .to_array_view::<f32>()?
+        .iter()
+        .cloned()
+        .zip(1..)
+        .sorted_by(|a, b| a.0.partial_cmp(&b.0).unwrap())
+        .rev()
+        .take(MAX_RESULTS)
+        .filter(|(confidence, _)| *confidence >= MIN_CONFIDENCE)
+        .map(|(confidence, label_idx)| (confidence, label_idx, labels[label_idx - 1]));
+
+    for (confidence, _, label) in sorted {
+        println!("{label} ({:.2}%)", (confidence * 100.0));
+    }
+    // let best = sorted.last();
+    // println!("result: {:?}", best);
+    Ok(())
+}
