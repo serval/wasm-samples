9504303021462c999385ce49f714a8d78d439835
Mark Christian <m@rkchristian.ca>
2023-01-31 09:22:05 -0800

    Update sdk-test to match latest sdk

--- sdk-test/src/main.rs
+++ sdk-test/src/main.rs
@@ -1,8 +1,20 @@
-use serval::invoke_capability;
+use serval::invoke_extension;
+
+#[link(wasm_import_module = "as-fib")]
+extern "C" {
+    fn fib(n: i32) -> i32;
+}
 
 fn main() {
+    println!("Calling a WASM extension...");
+    for i in 1..10 {
+        println!("Fibonacci #{i} = {}", unsafe { fib(i) });
+    }
+
+    println!("");
+
     println!("Invoking the birdfeeder capability...");
-    match invoke_capability("birdfeeder".to_string(), &"forks".as_bytes().to_vec()) {
+    match invoke_extension("birdfeeder".to_string(), &"forks".as_bytes().to_vec()) {
         Ok(buf) => {
             let resp = String::from_utf8_lossy(&buf);
             println!("Got response from host: {resp}");
